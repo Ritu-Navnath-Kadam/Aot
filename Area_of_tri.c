0ab468412997bb17620a358cc47586ea27e7f5b1
Ritu-Navnath-kadam <144406164+Ritu-Navnath-Kadam@users.noreply.github.com>
2023-10-23 15:33:48 +0530

    Create Area_of_tri.c

---
+++ Area_of_tri.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main(){
+
+float b,a,h;
+printf("Enter the value of breadth");
+scanf("%f",&b);
+printf("Enter the value of height");
+scanf("%f",&h);
+
+a=0.5*(b*h);
+printf("The area of  triangle is =%f",a);
+
+
+
+}
